5b7f68f39f3cd9937479451fec5ab9a4c1157219
Nathan Rogers <nrogers@clevelandart.org>
2017-12-07 13:32:52 -0500

    Generate reports for conservation directories and files

--- bin/analyze-conservation-files.rb
+++ bin/analyze-conservation-files.rb
@@ -6,59 +6,11 @@
 # by archivists and stakeholders
 #
 # To use
-# ./analyze-conservation-files.rb [root path to staging]
+# ./analyze-conservation-files.rb --path=[root path to staging]
+$:.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
 require 'csv'
 require 'optparse'
-require 'pry'
-
-SHORT_CODES = {
-  CT: "Conservation Treatment",
-  CL: "Conservation Loan",
-  CR: "Conservation Research",
-  CI: "Conservation Installation",
-  CA: "Conservation Analysis",
-  CE: "Conservation Acquisition",
-  CX: "Conservation Examination",
-  CER: "Conservation External Research",
-  APP: "Apparatus",
-  FR: "Frame",
-  BT: "Before Treatment",
-  DT: "During Treatment",
-  AT: "After Treatment",
-  SP: "Sample",
-  BS: "Before Sample",
-  AS: "After Sample",
-  REC: "Recto",
-  VER: "Verso",
-  TOP: "Top",
-  BOT: "Bottom",
-  FNT: "Front",
-  BCK: "Back",
-  LFT: "Left side of an object",
-  RGT: "Right side of an object",
-  INT: "Interior shot of an object",
-  EXT: "Exterior shot of an object",
-  # DEG_xxx: "Degree",
-  # DETxx: "Detail XX",
-  # SECxx: "Section XX",
-  DIA: "Diagram",
-  HEAD: "Head (book)",
-  SPINE: "Spine (book)",
-  TAIL: "Tail (book)",
-  FOREEDGE: "Fore edge (book)",
-  # PGxx: "Page XX",
-  NL: "Normal light",
-  RL: "Raking light",
-  TL: "Transmitted light",
-  SL: "Specular light",
-  OSL: "Oblique Specular Light",
-  UV: "Ultra Violet fluoresence",
-  IR: "Infrared Reflectography",
-  XR: "X-Ray",
-  RF: "Reflected",
-  FCUV: "False Color Ultraviolet",
-  FCIR: "False Color Infrared"
-}
+require 'conservation_codes'
 
 def normalize(path)
   path.gsub!(" ","_") 
@@ -76,20 +28,24 @@ end
 def parsePath(path)
   metadata = {}
   parts = path.split("_")
-  
+  title = []
+ 
   if parts[0].match(/^\d{2,}\.\d+/)
     metadata[:object] = parts.shift
   end
   
-  metadata[:codes] = [] 
-  parts.each do |code|
-	break if SHORT_CODES[code.to_sym].nil?
-	
-	code = parts.shift
-	metadata[:codes] << "#{SHORT_CODES[code.to_sym]} (#{code})"
-  end
+  metadata[:codes] = {}
   
-  metadata[:title] = parts.join(" ")
+  parts.each_with_index do |code, i|
+    if Conservation::SHORT_CODES[code.to_sym].nil?
+      title << code
+    else
+      expanded_field = Conservation::SHORT_CODES[code.to_sym]
+      metadata[:codes][expanded_field[1]] ||= []
+      metadata[:codes][expanded_field[1]] << "#{expanded_field[0]} (#{code})"
+    end
+  end
+  metadata[:title] = title.join(" ")
   
   return metadata
 end
@@ -106,52 +62,86 @@ directories = []
 files = [] 
 
 puts "Processing #{options[:path]}"
+
 Dir["#{options[:path]}/**/*"].each do |full_path|
   normalized_path = full_path.sub(options[:path], "")
   normalized_path = normalize(normalized_path)
-  
+
   directories << normalized_path if File.directory?(full_path)
   files << normalized_path if File.file?(full_path)
 end
 
-report = CSV.open("conservation-report.csv", "w+")
-report << ["Path", "Type", "Department", "Accession Number", "Title", "Codes"]
+directory_report = CSV.open("conservation-directories.csv", "w")
+headers = ["Path", "Accession Number", "Title"]
+Conservation::FIELDS.each { |f| headers << f.to_s }
+directory_report << headers
 
 # Start with directories
 directories.each do |dirname|
   paths = dirname.split("/") 
   paths.shift 
   
-  details = [dirname, "directory"]
-  details << paths.shift.split("_").join(" ") 
+  details = [dirname]
+  
+  metadata = {
+    codes: 
+      { division: [paths.shift.split("_").join(" ")]
+    }
+  } 
 
   while not paths.empty?
     segment = paths.shift
-    metadata = parsePath(segment) 
-    
-    if (not metadata[:object].nil?)
-     details << metadata[:object]
-     details << metadata[:title]
-     details << metadata[:codes].join("|")
-     break
+    properties = parsePath(segment) 
+   
+    if (not properties[:object].nil?)
+      metadata[:object] = properties[:object]
+      metadata[:title] = properties[:title] if metadata[:title].nil?
+
+      properties[:codes].each do |field, value|
+        metadata[:codes][field] ||= []
+        metadata[:codes][field] += value
+        metadata[:codes][field].uniq!
+      end
     end
   end
   
-  report << details
+  details << metadata[:object]
+  details << metadata[:title]
+
+  Conservation::FIELDS.each do |k| 
+    if metadata[:codes][k].nil?
+      details << ""
+    else
+      details << metadata[:codes][k].join(" | ") 
+    end
+  end
+
+  directory_report << details
 end
+directory_report.close()
 
 # Now process files
+file_report = CSV.open("conservation-files.csv", "w")
+headers = ["Path", "Accession Number", "Title"]
+Conservation::FIELDS.each { |f| headers << f.to_s }
+file_report << headers
+
 files.each do |path|
-  details = [path, "file"]
+  details = [path]
+  department = path.split("/")[1].gsub("_", " ")
 
   file_name = File.basename(path)
-  metadata = parsePath(file_name)
-
+  metadata = parsePath(File.basename(file_name, File.extname(file_name)))
+ 
   details << (metadata[:object].nil? ? "" : metadata[:object])
   details << file_name
-  details << metadata[:codes].join("|")
+  metadata[:codes][:division] = [department]
 
-  report << details
+  Conservation::FIELDS.each do |field|
+    value = metadata[:codes][field].nil? ?
+      "" : metadata[:codes][field].join(" | ")
+    details << value
+  end
+  file_report << details
 end
-
-report.close()
+file_report.close()
---
+++ lib/conservation_codes.rb
@@ -0,0 +1,83 @@
+# Short codes for Conservation codes 
+# 
+# These codes are used by the Conservation department in file names to
+# indicates various attributes of an image such as lighting, technique,
+# stage, or location within the object. To use these simply require the
+# library in any scripts that need to look up the values
+module Conservation
+  FIELDS = [
+    :division, # Lab within the Conservation department,
+    :conservation_type, # Category for image(s)
+    :conservation_state, # Point at which image(s) are being captured
+    :component, # Part of the obect being photographed
+    :lighting, # Lighting methods used in image capture
+    :sample_id, # Sequential identifier for samples
+    :technique, # Photographic techiques used for capture
+  ]
+
+  SHORT_CODES = {
+    CT: ["Conservation Treatment", :conservation_type],
+    CL: ["Conservation Loan", :conservation_type],
+    CR: ["Conservation Research", :conservation_type],
+    CI: ["Conservation Installation", :conservation_type],
+    CA: ["Conservation Analysis", :conservation_type],
+    CE: ["Conservation Acquisition", :conservation_type],
+    CX: ["Conservation Examination", :conservation_type],
+    CER: ["Conservation External Research", :conservation_type],
+    APP: ["Apparatus", :conservation_type],
+    FR: ["Frame", :conservation_type],
+
+    BT: ["Before Treatment", :conservation_state],
+    DT: ["During Treatment", :conservation_state],
+    AT: ["After Treatment", :conservation_state],
+    SP: ["Sample", :conservation_state],
+    BS: ["Before Sample", :conservation_state],
+    AS: ["After Sample", :conservation_state],
+
+    REC: ["Recto", :component],
+    VER: ["Verso", :component],
+    TOP: ["Top", :component],
+    BOT: ["Bottom", :component],
+    FNT: ["Front", :component],
+    BCK: ["Back", :component],
+    LFT: ["Left side of an object", :component],
+    RGT: ["Right side of an object", :component],
+    INT: ["Interior shot of an object", :component],
+    EXT: ["Exterior shot of an object", :component],
+    # DEG_xxx: "Degree",
+    # DETxx: "Detail XX",
+    # SECxx: "Section XX",
+    DIA: ["Diagram", :component],
+    HEAD: ["Head (book)", :component],
+    SPINE: ["Spine (book)", :component],
+    TAIL: ["Tail (book)", :component],
+    FOREEDGE: ["Fore edge (book)", :component],
+    # PGxx: "Page XX",
+
+    NL: ["Normal light", :lighting],
+    RL: ["Raking light", :lighting],
+    TL: ["Transmitted light", :lighting],
+    SL: ["Specular light", :lighting],
+    OSL: ["Oblique Specular Light", :lighting],
+    UV: ["Ultra Violet fluoresence", :lighting],
+    IR: ["Infrared Reflectography", :lighting],
+    XR: ["X-Ray", :lighting],
+    RF: ["Reflected", :lighting],
+    FCUV: ["False Color Ultraviolet", :lighting],
+    FCIR: ["False Color Infrared", :lighting],
+
+    # SPx: "Sample XX",
+  
+    MIC: ["Microscopy", :technique],
+    XRF: ["X-ray Flourescence Spectroscopy", :technique],
+    FTIR: ["Fourier Transform Infrared Spectroscopy", :technique],
+    XRD: ["X-ray Diffraction", :technique],
+    MICRO: ["Microscope Photos", :technique],
+    SEM: ["Scanning Electron Microscope", :technique],
+    SEMEDS: ["Scanning Electron Microscopy with Energy Dispersive Spectroscopy", :technique],
+    GCMS: ["Gas Chromatography, Mass Spectroscopy", :technique],
+    TLD: ["Thermo Luminescence Dating", :technique],
+    HPLC: ["High Performance Liquid Chromotography", :technique],
+    C14: ["Carbon 14 Dating", :technique]
+  }
+end
